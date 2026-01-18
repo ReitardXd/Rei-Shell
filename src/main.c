9661747990656139dd5455677739b3c84e71831f
Pranav Roy <ReitardXd>
2026-01-18 20:16:50 +0530

    gg

--- src/main.c
+++ src/main.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
-
-  // TODO: Uncomment the code below to pass the first stage
   printf("$ ");
+ char input[100];
+ fgets(input, sizeof(100),stdin);
+ 
+ input [strlen(input)  - 1  ]  = '\0';
+ printf ( "%s : input not found\n", input);
 
   return 0;
 }
