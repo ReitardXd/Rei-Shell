cf254de200715f48f8f8be8f1b5f6095121418e4
Pranav Roy <ReitardXd>
2026-01-18 20:17:27 +0530

    gg

--- src/main.c
+++ src/main.c
@@ -8,8 +8,8 @@ int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
   printf("$ ");
- char input[100];
- fgets(input, sizeof(100),stdin);
+ char input[1024];
+ fgets(input, sizeof(1024),stdin);
  
  input [strlen(input)  - 1  ]  = '\0';
  printf ( "%s : input not found\n", input);
